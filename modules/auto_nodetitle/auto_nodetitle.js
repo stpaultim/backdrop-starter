5aed7a7733a0d6c0a8b7a518469be8d3dd8f0d1d
Tim Erickson <tim@triplo.co>
2018-05-25 00:28:46 -0500

    Adding Module: Auto Node Title

---
+++ modules/auto_nodetitle/auto_nodetitle.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+Backdrop.behaviors.auto_nodetitleFieldsetSummaries = {
+  attach: function (context) {
+    $('fieldset#edit-auto-nodetitle', context).backdropSetSummary(function (context) {
+
+      // Retrieve the value of the selected radio button
+      var ant = $("input[name=#edit-auto-nodetitle-ant]:checked").val();
+
+      if (ant==0) {
+        return Backdrop.t('Disabled')
+      }
+      else if (ant==1) {
+        return Backdrop.t('Automatic (hide title field)')
+      }
+      else if (ant==2) {
+        return Backdrop.t('Automatic (if title empty)')
+      }
+    });
+  }
+};
+
+})(jQuery);
