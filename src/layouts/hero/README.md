3231e644125fb3fd86a7cd4662ae65c3f251cfe7
Tim Erickson <tim@triplo.co>
2019-07-04 23:48:12 -0500

    Adding Layouts

---
+++ src/layouts/hero/README.md
@@ -0,0 +1,33 @@
+Hero layouts
+=======================
+
+Versions of core layouts with container classes inside the wrapper.
+
+Usage
+-----
+
+This attempts to resolve a limitaion of the core layouts in that it allows any 
+region to be made full width while allowing other regions to be fixed-width. 
+Regions can be made full width by over-riding the `.container` class for that 
+region.
+
+Example:
+`.l-middle .container { width: 100%; }`
+(will also require over-riding the margins.)
+
+This project also includes a new Hero region configured for core Hero Blocks 
+to display full-width without further CSS adjustments.
+
+
+License
+-------
+
+This project is GPL v2 software. See the LICENSE.txt file in this directory for
+complete text.
+
+Maintainers
+-----------
+
+- Wilmoth Shillingford (https://github.com/docwilmot/)
+
+This module is seeking additional maintainers.
---
+++ src/layouts/hero/simmons_hero/layout--simmons-hero.tpl.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * @file
+ * Template for the Simmons Hero layout.
+ *
+ * Variables:
+ * - $title: The page title, for use in the actual HTML content.
+ * - $messages: Status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node.)
+ * - $action_links: Array of actions local to the page, such as 'Add menu' on
+ *   the menu administration interface.
+ * - $classes: Array of CSS classes to be added to the layout wrapper.
+ * - $attributes: Array of additional HTML attributes to be added to the layout
+ *     wrapper. Flatten using backdrop_attributes().
+ * - $content: An array of content, each item in the array is keyed to one
+ *   region of the layout. This layout supports the following sections:
+ *   - $content['header']
+ *   - $content['nav']
+ *   - $content['hero']
+ *   - $content['top']
+ *   - $content['content']
+ *   - $content['sidebar']
+ *   - $content['sidebar2']
+ *   - $content['third1']
+ *   - $content['third2']
+ *   - $content['third3']
+ *   - $content['quarter1']
+ *   - $content['quarter2']
+ *   - $content['quarter3']
+ *   - $content['quarter4']
+ *   - $content['bottom']
+ *   - $content['footer']
+ */
+?>
+<div class="hero-layouts layout--simmons-hero <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <?php if ($content['header']): ?>
+    <header class="l-header" role="banner" aria-label="<?php print t('Site header'); ?>">
+      <div class="l-header-inner container container-fluid row">
+        <?php print $content['header']; ?>
+      </div>
+    </header>
+  <?php endif; ?>
+  <?php if ($content['nav']): ?>
+    <nav class="l-header l-nav" role="banner" aria-label="<?php print t('Site nav'); ?>">
+      <div class="l-nav-inner container container-fluid row">
+        <?php print $content['nav']; ?>
+      </div>
+    </nav>
+  <?php endif; ?>
+
+  <div class="l-wrapper">
+
+    <?php if ($messages): ?>
+      <div class="l-messages" role="status" aria-label="<?php print t('Status messages'); ?>">
+        <div class="container container-fluid row">
+          <?php print $messages; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <div class="l-page-title">
+      <div class="container container-fluid row">
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+      </div>
+    </div>
+
+    <?php if ($tabs): ?>
+      <nav class="tabs" role="tablist" aria-label="<?php print t('Admin content navigation tabs.'); ?>">
+        <div class="container container-fluid row">
+          <?php print $tabs; ?>
+        </div>
+      </nav>
+    <?php endif; ?>
+
+    <div class="container container-fluid row">
+      <?php print $action_links; ?>
+    </div>
+
+    <?php if (!empty($content['hero'])): ?>
+      <div class="l-hero">
+        <div class="no-container">
+          <?php print $content['hero']; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!empty($content['top'])): ?>
+      <div class="l-top">
+        <div class="container container-fluid row">
+          <?php print $content['top']; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <div class="l-middle">
+      <div class="container container-fluid row">
+        <main class="l-content col-md-6 col-md-push-3" role="main" aria-label="<?php print t('Main content'); ?>">
+          <?php print $content['content']; ?>
+        </main>
+        <div class="l-sidebar l-sidebar-first col-md-3 col-md-pull-6">
+          <?php print $content['sidebar']; ?>
+        </div>
+        <div class="l-sidebar l-sidebar-second col-md-3">
+          <?php print $content['sidebar2']; ?>
+        </div>
+      </div>
+    </div><!-- /.l-middle -->
+
+    <?php if ($content['third1'] || $content['third2'] || $content['third3']): ?>
+      <div class="l-thirds row">
+        <div class="container container-fluid row">
+          <div class="l-thirds-region col-md-4">
+            <?php print $content['third1']; ?>
+          </div>
+          <div class="l-thirds-region col-md-4">
+            <?php print $content['third2']; ?>
+          </div>
+          <div class="l-thirds-region col-md-4">
+            <?php print $content['third3']; ?>
+          </div>
+        </div>
+      </div><!-- /.l-thirds -->
+    <?php endif; ?>
+
+    <?php if ($content['quarter1'] || $content['quarter2'] || $content['quarter3'] || $content['quarter4']): ?>
+      <div class="l-quarters row">
+        <div class="container container-fluid row">
+          <div class="l-quarters-region col-md-3">
+            <?php print $content['quarter1']; ?>
+          </div>
+          <div class="l-quarters-region col-md-3">
+            <?php print $content['quarter2']; ?>
+          </div>
+          <div class="l-quarters-region col-md-3">
+            <?php print $content['quarter3']; ?>
+          </div>
+          <div class="l-quarters-region col-md-3">
+            <?php print $content['quarter4']; ?>
+          </div>
+        </div>
+      </div><!-- /.l-quarters -->
+    <?php endif; ?>
+
+    <?php if (!empty($content['bottom'])): ?>
+      <div class="l-bottom">
+        <div class="container container-fluid row">
+          <?php print $content['bottom']; ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+  </div><!-- /.l-wrapper -->
+
+  <?php if ($content['footer']): ?>
+    <footer class="l-footer"  role="footer">
+      <div class="l-footer-inner container container-fluid row">
+        <?php print $content['footer']; ?>
+      </div>
+    </footer>
+  <?php endif; ?>
+</div><!-- /.layout--simmons-hero -->
